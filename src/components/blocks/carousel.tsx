3da5003f8a7a74b3eb655ebb88bcc59c3ad9d26e
Hubert Lin <linhub15@users.noreply.github.com>
2025-06-05 13:47:50 -0500

    feat: finish carousel

--- src/components/blocks/carousel.tsx
+++ src/components/blocks/carousel.tsx
@@ -8,21 +8,23 @@ import type { EmblaCarouselType, EmblaOptionsType } from "embla-carousel";
 import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
 
-// todo: slide should accept more data
 type Slide = {
   image: string;
+  alt: string;
   title: string;
   caption: string;
 };
 
 type Props = {
-  slides: number[];
+  slides: Slide[];
   options?: EmblaOptionsType;
 };
 
 function Carousel(props: Props) {
   const { slides, options } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()]);
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    Autoplay({ delay: 3000 }),
+  ]);
 
   const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
     const autoplay = emblaApi?.plugins()?.autoplay;
@@ -42,33 +44,46 @@ function Carousel(props: Props) {
 
   return (
     <section>
-      <div className="overflow-hidden" ref={emblaRef}>
+      <div className="relative overflow-hidden" ref={emblaRef}>
         <div className="flex">
-          {slides.map((index) => (
+          {slides.map((slide) => (
             <div
               className="flex-[0_0_var(--slide-size)] pl-[var(--slide-spacing)]min-w-0"
-              key={index}
+              key={slide.title}
             >
-              <div className="shadow-[inset_0_0_0_0.2rem_var(--detail-medium-contrast)] text-6xl font-semibold flex items-center justify-center h-[var(--slide-height)] select-none">
-                {index + 1}
+              <div className="relative h-[var(--slide-height)] select-none">
+                <img
+                  className="size-full object-fill"
+                  src={slide.image}
+                  height="300"
+                  alt=""
+                />
+                <div className="absolute inset-0 shadow-[inset_0_0_0_0.2rem_var(--detail-medium-contrast)] flex items-center justify-center z-10">
+                  <div className="flex flex-col items-end">
+                    <span className="px-4 py-1.5 text-3xl max-w-md font-semibold uppercase bg-brand-600/85 text-white">
+                      {slide.title}
+                    </span>
+                    <span className="px-3 py-1.5 text-2xl italic max-w-md bg-white/70">
+                      {slide.caption}
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
         </div>
-      </div>
 
-      <div className="grid grid-cols-[auto_1fr] justify-between gap-4 mt-6">
-        <div className="flex flex-wrap justify-end items-center -mr-2">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={"flex items-center justify-center bg-black touch-manipulation no-underline cursor-pointer border-0 p-0 m-0 size-10 rounded-full appearance-none"
-                .concat(
-                  index === selectedIndex ? " embla__dot--selected" : "",
-                )}
-            />
-          ))}
+        <div className="absolute hidden md:flex bottom-0 py-3 gap-4 w-full justify-center bg-linear-to-t from-black/50 to-black/0">
+          <div className="flex gap-2.5 items-center">
+            {scrollSnaps.map((_, index) => (
+              <DotButton
+                key={index}
+                onClick={() => onDotButtonClick(index)}
+                className={"flex items-center justify-center border-3 border-white touch-manipulation no-underline cursor-pointer p-0 m-0 size-6 rounded-full appearance-none data-[selected]:bg-white"}
+                data-selected={index === selectedIndex ? "" : undefined}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>

acf250e57cb07ed7dbca88e5f5f5b0106d4328c0
Hubert Lin <linhub15@users.noreply.github.com>
2025-07-08 17:26:01 -0600

    feat: update facebook link

--- src/consts.ts
+++ src/consts.ts
@@ -8,7 +8,7 @@ export const SITE_DESCRIPTION =
 export const SITE_CONTACT = {
   phone: "780-708-1401",
   email: "anna@sanicleanltd.com",
-  facebook: "https://www.facebook.com/profile.php?id=100067633419677",
+  facebook: "https://www.facebook.com/profile.php?id=61578093994534",
   linkedin: "https://www.linkedin.com/company/107561796",
 };
 
